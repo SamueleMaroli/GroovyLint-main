0bfeef8fe554b2aac547df3ccad4185413f9b4ee
SamueleMaroli <133371511+SamueleMaroli@users.noreply.github.com>
2024-03-20 15:55:09 +0100

    lll

---
+++ GroovyLint-main/Dockerfile
@@ -0,0 +1,1 @@
+FROM nvuillam/npm-groovy-lint:latest
---
+++ GroovyLint-main/ThinkOfANumber2.groovy
@@ -0,0 +1,33 @@
+/**
+ * Classe per pensare a un numero e fare indovinare all'utente. Versione 2
+ */
+@CompileStatic
+class ThinkOfANumber2 {
+
+    static void main(String[] args) {
+        // Genera un numero casuale tra 0 e 5
+        int randomNum = new SecureRandom().nextInt(6)
+        println 'The computer has chosen a number between 0 and 5. Can you guess it?'
+
+
+
+
+
+
+
+
+        // Ottiene l'input dell'utente
+        Scanner scanner = new Scanner(System.in)
+        int userGuess = scanner.nextInt()
+
+        // Controlla se l'indovinello è corretto
+        if (userGuess == randomNum) {
+            println 'Well done!'
+        } else {
+            println "Tough luck - you're wrong"
+        }
+
+        scanner.close() // Chiudi lo scanner per evitare perdite di risorse
+    }
+
+}
